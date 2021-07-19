e352123cd90dd74d91c1ede759b170babbe2f4bc
Mo Almishkawi <mo.almishkawi@gmail.com>
2021-07-19 14:57:52 +0200

    remove context path

--- src/main/java/com/mo/JWTAuth/web/controller/UserController.java
+++ src/main/java/com/mo/JWTAuth/web/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("api/users")
 public class UserController {
     @Autowired
     private ModelMapper modelMapper;
